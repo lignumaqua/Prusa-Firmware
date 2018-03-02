d50d2ac62e34fc4b86ce9d1054162e7b6d60ee39
lignumaqua <mike@mikewoodconsulting.com>
2018-03-02 13:37:47 -0600

    Increse current

--- Firmware/Configuration.h
+++ Firmware/Configuration.h
@@ -11,7 +11,7 @@
 #define FW_COMMIT_NR   224
 // FW_VERSION_UNKNOWN means this is an unofficial build.
 // The firmware should only be checked into github with this symbol.
-#define FW_DEV_VERSION FW_VERSION_UNKNOWN
+#define FW_DEV_VERSION FW_VERSION_RC
 #define FW_REPOSITORY "Prusa3D/MK3"
 #define FW_VERSION_FULL FW_VERSION "-" STR(FW_COMMIT_NR)
 
